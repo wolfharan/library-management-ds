8869684f206d0621fa5f2e476842d2ea3240cb42
Hari Charan U <31123971+wolfharan@users.noreply.github.com>
2017-10-01 22:06:40 +0530

    Update client.c
    //srno- serial number
    //nob-name of the book
    //auth-author
    //sub-subject
    //tnoc-total number of copies
    //lnoc=lent number of copies

--- client.c
+++ client.c
@@ -12,12 +12,6 @@ typedef struct node
   struct node *next;
   struct node *prev;
 }NODE;
-//srno- serial number
-//nob-name of the book
-//auth-author
-//sub-subject
-//tnoc-total number of copies
-//lnoc=lent number of copies
 
 typedef struct dlist
 {
