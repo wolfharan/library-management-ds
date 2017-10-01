5895209ae73465a79fb1e5da8c241e326f7f4e36
Hari Charan U <31123971+wolfharan@users.noreply.github.com>
2017-10-01 22:06:18 +0530

    Update client.c

--- client.c
+++ client.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
+#include <string.h>
 typedef struct node
 {
   int srno;
-  char nob[10];
+  char name[10];
   char auth[10];
   char branch[10];
   char sub[10];
@@ -24,3 +25,19 @@ typedef struct dlist
   NODE *tail;
   int nbooks;
  }DLIST;
+
+int addbook(DLIST *l,char name[10],char auth[10],char branch[10],char sub[10],int tnoc)
+{
+  NODE *newbook=(NODE *)malloc(sizeof(NODE));
+  strcpy(newbook->name,name);
+  strcpy(newbook->auth,auth);
+  strcpy(newbook->branch,branch);
+  strcpy(newbook->sub,sub);
+  newbook->tnoc=tnoc;
+  newbook->lnoc=0;
+  NODE *temp=l->tail;
+  temp->next=newbook;
+  newbook->prev=temp;
+}
+  
+    
