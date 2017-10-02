2e3348e584b06f9eec2cec2f94fc445e1f14339a
Hari Charan U <31123971+wolfharan@users.noreply.github.com>
2017-10-02 17:21:57 +0530

    Update server.c

--- server.c
+++ server.c
@@ -23,15 +23,25 @@ typedef struct dlist
 int addbook(DLIST *l,char name[10],char auth[10],char branch[10],char sub[10],int tnoc)
 {
   NODE *newbook=(NODE *)malloc(sizeof(NODE));
-  strcpy(newbook->name,name);
-  strcpy(newbook->auth,auth);
-  strcpy(newbook->branch,branch);
-  strcpy(newbook->sub,sub);
-  newbook->tnoc=tnoc;
-  newbook->lnoc=0;
-  NODE *temp=l->tail;
-  temp->next=newbook;
-  newbook->prev=temp;
+  if(newbook==NULL)
+  {
+    return 0;
+  }
+  else
+  {
+    strcpy(newbook->name,name);
+    strcpy(newbook->auth,auth);
+    strcpy(newbook->branch,branch);
+    strcpy(newbook->sub,sub);
+    newbook->tnoc=tnoc;
+    newbook->lnoc=0;
+    NODE *temp=l->tail;
+    temp->next=newbook;
+    newbook->prev=temp;
+    l->tail=temp;
+    return 1;
+  }
 }
-  
+int lendbook(char name[10])
+
     
