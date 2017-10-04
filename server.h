393f0c5bc544585ed3c6fe34b1b414dd81538021
Hari Charan U <31123971+wolfharan@users.noreply.github.com>
2017-10-04 09:22:08 +0530

    Update server.h

--- server.h
+++ server.h
@@ -20,6 +20,20 @@ typedef struct bdlist
   int nbooks;
  }BDLIST;
 
+typedef struct tnode
+{
+  char name[4];
+  struct tnode *p1;
+  struct tnode *p2;
+  struct tnode *p3;
+  struct tnode *p4;
+}TNODE;
+
+typedef struct tree
+{
+  TNODE *head;
+}TREE;
+  
 int addbook(BDLIST *l,char name[10],char auth[10],char branch[10],char sub[10],int tnoc);
 int deletebook(BDLIST *l,char name[10]);
 
