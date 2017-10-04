ca7229bc806eb64d4bdb411c3fbf127b4de21220
Hari Charan U <31123971+wolfharan@users.noreply.github.com>
2017-10-04 08:55:45 +0530

    Update server.c

--- server.c
+++ server.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <string.h>
-typedef struct node
+typedef struct bnode
 {
   int srno;
   char name[10];
@@ -9,20 +9,20 @@ typedef struct node
   char sub[10];
   int tnoc;
   int lnoc;
-  struct node *next;
-  struct node *prev;
-}NODE;
+  struct bnode *next;
+  struct bnode *prev;
+}BNODE;
 
-typedef struct dlist
+typedef struct bdlist
 {
-  NODE *head;
-  NODE *tail;
+  BNODE *head;
+  BNODE *tail;
   int nbooks;
- }DLIST;
+ }BDLIST;
 
-int addbook(DLIST *l,char name[10],char auth[10],char branch[10],char sub[10],int tnoc)
+int addbook(BDLIST *l,char name[10],char auth[10],char branch[10],char sub[10],int tnoc)
 {
-  NODE *newbook=(NODE *)malloc(sizeof(NODE));
+  BNODE *newbook=(BNODE *)malloc(sizeof(BNODE));
   if(newbook==NULL)
   {
     return 0;
@@ -35,13 +35,82 @@ int addbook(DLIST *l,char name[10],char auth[10],char branch[10],char sub[10],in
     strcpy(newbook->sub,sub);
     newbook->tnoc=tnoc;
     newbook->lnoc=0;
-    NODE *temp=l->tail;
-    temp->next=newbook;
-    newbook->prev=temp;
-    l->tail=temp;
+    if(l->tail!=NULL)
+    {
+      BNODE *temp=l->tail;
+      int sr=temp->data;
+      newbook->srno=sr+1;
+      temp->next=newbook;
+      newbook->prev=temp;
+      newbook->next=NULL;
+      l->tail=temp;
+    }
+    else
+    {
+      l->head=newbook;
+      l->tail=newbook;
+      newbook->srno=1;
+      newbook->next=NULL;
+      newbook->prev=NULL;
+    }
+      
+    l->nbooks++; 
     return 1;
   }
 }
-int lendbook(char name[10])
-
+int deletebook(BDLIST *l,char name[10])
+{
+  if(l->head==NULL)
+  {
+    return 0;
+  }
+  BNODE *temp=l->head;
+  while(strcmp(l->name,name)!=0 && temp!=NULL)
+  {
+    temp=temp->next;
+  }
+  if(temp==NULL)
+  {
+    return 0;
+  }
+  
+  else
+  {
+    if(l->head!=temp)
+    {
+      
+      BNODE *temp1=temp->prev;
+      temp1->next=temp->next;
+      if(temp->next==NULL)
+      {
+        free(temp);
+        l->tail=temp1;
+      }    
+      else
+      {
+        temp->next->prev=temp1;
+        free(temp);
+      }
+    }
+    else
+    {
+      if(l->nbooks==1)
+      {
+        free(l->head);
+        l->head==NULL;
+        l->tail==NULL;
+      }
+      else
+      {
+        l->head->next->prev=NULL;
+        BNODE *temp2=l->head;
+        l->head=l->head->next;
+        free(temp2);
+      }
+      l->nbooks--;
+      return 1;
+    }
+        
     
+  
+  
