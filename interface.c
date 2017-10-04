a20ee192674aed0081a048d59fce1442b149e504
Hari Charan U <31123971+wolfharan@users.noreply.github.com>
2017-10-04 10:04:21 +0530

    Update interface.c

--- interface.c
+++ interface.c
@@ -1,19 +1,20 @@
 #include "server.h"
-
-TREE T;
-TNODE b1;
-TNODE b2;
-TNODE b3;
-TNODE b4;
-strcpy(b1.name,"CSE");
-strcpy(b2.name,"ECE");
-strcpy(b3.name,"EEE");
-strcpy(b4.name,"MECH");
-TNODE PES;
-T.head=&PES;
-PES.p1=&b1;
-PES.p2=&b2;
-PES.p3=&b3;
-PES.p4=&b4;
-
-
+ void main()
+ {
+   TREE *T=(TREE *)malloc(sizeof(TREE));
+   TNODE *PES=(TNODE *)malloc(sizeof(TNODE));
+   TNODE *b1=(TNODE *)malloc(sizeof(TNODE));
+   TNODE *b2=(TNODE *)malloc(sizeof(TNODE));
+   TNODE *b3=(TNODE *)malloc(sizeof(TNODE));
+   TNODE *b4=(TNODE *)malloc(sizeof(TNODE));
+   strcpy(b1->name,"CSE");
+   strcpy(b2->name,"ECE");
+   strcpy(b3->name,"EEE");
+   strcpy(b4->name,"MECH");
+   PES->p1=b1;
+   PES->p2=b2;
+   PES->p3=b3;
+   PES->p4=b4;
+   
+   
+   
