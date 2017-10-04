50572ff48b77b2c5429668445ebe5529762fef8b
Hari Charan U <31123971+wolfharan@users.noreply.github.com>
2017-10-04 10:12:50 +0530

    Update interface.c

--- interface.c
+++ interface.c
@@ -2,11 +2,11 @@
  void main()
  {
    TREE *T=(TREE *)malloc(sizeof(TREE));
-   TNODE *PES=(TNODE *)malloc(sizeof(TNODE));
-   TNODE *b1=(TNODE *)malloc(sizeof(TNODE));
-   TNODE *b2=(TNODE *)malloc(sizeof(TNODE));
-   TNODE *b3=(TNODE *)malloc(sizeof(TNODE));
-   TNODE *b4=(TNODE *)malloc(sizeof(TNODE));
+   TBNODE *PES=(TBNODE *)malloc(sizeof(TNODE));
+   TBNODE *b1=(TBNODE *)malloc(sizeof(TNODE));
+   TBNODE *b2=(TBNODE *)malloc(sizeof(TNODE));
+   TBNODE *b3=(TBNODE *)malloc(sizeof(TNODE));
+   TBNODE *b4=(TBNODE *)malloc(sizeof(TNODE));
    strcpy(b1->name,"CSE");
    strcpy(b2->name,"ECE");
    strcpy(b3->name,"EEE");
@@ -15,6 +15,25 @@
    PES->p2=b2;
    PES->p3=b3;
    PES->p4=b4;
+  SUBNODE *cs1=(SUBNODE *)malloc(sizeof(SUBNODE));
+  SUBNODE *cs2=(SUBNODE *)malloc(sizeof(SUBNODE));
+  SUBNODE *ec1=(SUBNODE *)malloc(sizeof(SUBNODE));
+  SUBNODE *ee1=(SUBNODE *)malloc(sizeof(SUBNODE));
+  SUBNODE *me1=(SUBNODE *)malloc(sizeof(SUBNODE));
+  b1->p1=cs1;
+  b1->p3=NULL;
+  b1->p4=NULL;
+  b1->p2=cs2;
+  b2->p1=ec1;
+  b2->p2=NULL;
+  b2->p3=NULL;
+  b2->p4=NULL;
+  b3->p1=ee1;
+  b3->p2=NULL;
+  b3->p3=NULL;
+  b3->p4=NULL;
+  
+  b4->p1=me1;
    
    
    
