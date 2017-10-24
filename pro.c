10823af761936531bf2369f9e3a6b64e4fe1e038
Hari Charan U <31123971+wolfharan@users.noreply.github.com>
2017-10-25 00:54:08 +0530

    Update pro.c

--- pro.c
+++ pro.c
@@ -1,1 +1,62 @@
+#include <stdio.h>
+#include <string.h>
 
+typedef struct book{
+  char name[10];
+  char author[10];
+  int srno;
+  int noc[5];
+  struct book* next;
+  struct book* prev;
+}book;
+
+typedef struct subject{
+  char name[10];
+  book* start;
+  book* end;
+  int nob;
+}subject;
+
+int create_sub(subject* sub,char name[10])
+{
+  sub=(subject*)malloc(sizeof(subject));
+  if(sub==NULL)
+  {
+    return 0;
+  }
+  strcpy(name,sub->name);
+  sub->start=NULL;
+  sub->end=NULL;
+  nob=0;
+  return 1;
+}
+
+int addbook(char name[10],char author[10],int srno,subject *sub)
+{
+  book* temp=(book*)malloc(sizeof(book));
+  if(temp==NULL)
+  {
+    return 0;
+  }
+  
+  strcpy(name,temp->name);
+  strcpy(author,temp->author);
+  temp->srno=srno;
+  temp->next=NULL;
+  temp->prev=NULL;
+  if(sub->start==NULL)
+  {
+    sub->start=temp;
+    sub->end=temp;
+    nob++;
+    return 1;
+  }
+  else
+  {
+    sub->end->next=temp;
+    temp->prev=sub->end;
+    sub->end=temp;
+    nob++;
+    return 1;
+  }
+}
